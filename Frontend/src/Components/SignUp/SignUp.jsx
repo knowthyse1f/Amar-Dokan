2a0a36f8a5cec2c7b313ffaa2e43b22e876ca431
UthfolGhosh <uthfolghosh9038@gmail.com>
2024-07-06 16:04:34 +0600

    login and sign up page upload

--- Frontend/src/Components/SignUp/SignUp.jsx
+++ Frontend/src/Components/SignUp/SignUp.jsx
@@ -1,16 +1,29 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
+import { RxAvatar } from "react-icons/rx";
+import styles from "../../styles/styles";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
-  const [visible, setVisible] = useState("false");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [avatar, setAvatar] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    setAvatar(file);
+  };
 
   return (
-    <div className=" min-h-screen bg-gray-50 flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Create A New Account
@@ -18,23 +31,23 @@ const SignUp = () => {
       </div>
       <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6">
-          <div>
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
               <label
-                htmlFor="text"
+                htmlFor="name"
                 className="block text-sm font-medium text-gray-700"
               >
-               Full Name
+                Full Name
               </label>
-              <div className="at-1">
+              <div className="mt-1">
                 <input
                   type="text"
-                  name="text"
+                  name="name"
                   autoComplete="name"
                   required
                   value={name}
                   onChange={(e) => setName(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border borber-gray-300 rounded-md placeholder-gray-400 focus-within:outline-none focus:ring-blue-500 focue:border-blue-500 sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
               </div>
             </div>
@@ -45,7 +58,7 @@ const SignUp = () => {
               >
                 Email Address
               </label>
-              <div className="at-1">
+              <div className="mt-1">
                 <input
                   type="email"
                   name="email"
@@ -53,7 +66,27 @@ const SignUp = () => {
                   required
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border borber-gray-300 rounded-md placeholder-gray-400 focus-within:outline-none focus:ring-blue-500 focue:border-blue-500 sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="phoneNumber"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Phone Number
+              </label>
+              <div className="mt-1">
+                <input
+                  type="tel"
+                  name="phoneNumber"
+                  autoComplete="tel"
+                  required
+                  value={phoneNumber}
+                  pattern="01[0-9]{9}"
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
               </div>
             </div>
@@ -64,7 +97,7 @@ const SignUp = () => {
               >
                 Password
               </label>
-              <div className="at-1 relative">
+              <div className="mt-1 relative">
                 <input
                   type={visible ? "text" : "password"}
                   name="password"
@@ -72,7 +105,7 @@ const SignUp = () => {
                   required
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border borber-gray-300 rounded-md placeholder-gray-400 focus-within:outline-none focus:ring-blue-500 focue:border-blue-500 sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
                 {visible ? (
                   <AiOutlineEye
@@ -89,40 +122,50 @@ const SignUp = () => {
                 )}
               </div>
             </div>
-            <div className="flex items-center justify-between ">
-              <div className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="remember-me"
-                  id="remember-me"
-                  className="h-4 w-4 text-blue-600 border-gray-300 rounded"
-                />
+            <div>
+              <label
+                htmlFor="avatar"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Avatar
+              </label>
+              <div className="mt-2 flex items-center">
+                <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
+                  {avatar ? (
+                    <img
+                      src={URL.createObjectURL(avatar)}
+                      alt="avatar"
+                      className="object-cover h-full w-full rounded-full"
+                    />
+                  ) : (
+                    <RxAvatar className="h-8 w-8" />
+                  )}
+                </span>
                 <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
+                  htmlFor="file-input"
+                  className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md text-sm text-gray-700 bg-white hover:bg-gray-500"
                 >
-                  Remember me
+                  <span>Upload a file</span>
+                  <input
+                    type="file"
+                    name="avatar"
+                    id="file-input"
+                    accept=".jpg,.png,.jpeg"
+                    onChange={handleFileInputChange}
+                    className="sr-only"
+                  />
                 </label>
-              </div>
-              <div className="text-sm">
-                <a
-                  href=".forgetpass"
-                  className=" font-medium text-blue-400 hover:text-blue-300"
-                >
-                  Forget your password?
-                </a>
               </div>
             </div>
             <div>
-              <button type='submit'
-              className="custom-button"
-              >
-               Submit </button>  
+              <button type="submit" className="custom-button">
+                Sign Up
+              </button>
             </div>
-            <div className="flex items-center justify-between ">
-              <h4>Not have any Account?</h4>
-              <Link to="/sign-up" className="text-blue-500 ">
-              Sign Up
+            <div className="flex items-center justify-between">
+              <h4>Already have an account?</h4>
+              <Link to="/login" className="text-blue-500">
+                Sign In
               </Link>
             </div>
           </form>
